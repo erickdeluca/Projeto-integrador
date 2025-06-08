b33748f2c06b2c8e473b031fda08ca725e405fe0
Erick de Luca <delucaerick@gmail.com>
2025-06-08 10:10:36 -0300

    retira elementos de POO

--- sketch.js
+++ sketch.js
@@ -1,60 +1,49 @@
-let speed;
-class Star {
-    constructor() {
-        this.x = random(-width, width);
-        this.y = random(-height, height);
-        this.z =  random(width);
-        this.pz = this.z;
-    }
-
-    update(){
-        this.z -= speed;
-        if(this.z < 1){
-            this.z = width;
-            this.x = random(-width, width);
-            this.y = random(-height, height);
-            this.pz = this.z;
-
-        }
-    }
-    
-    show() {
-        let raio = 2;
-        fill(255); 
-        noStroke();
-
-        let sx = map(this.x / this.z, 0, 1, 0, width);
-        let sy = map(this.y / this.z, 0, 1, 0, width);
-
-        let r = map(this.z, 0, width, 16, 0 );
-        // circle(sx, sy, r);
-
-        let px = map(this.x / this.pz, 0, 1, 0, width);
-        let py = map(this.y / this.pz, 0, 1, 0, height);
-
-        this.pz = this.z;
-        stroke(255);
-        line(px, py, sx, sy);
-    }
-}
-
-let stars =  [];
+let stars = [];
+let speed = 0;
 
 function setup() {
-    createCanvas(800, 800);
-    for(let i = 0; i < 800; i++){
-      stars[i] = new Star();
-    }
+  createCanvas(800, 800);
+  for (let i = 0; i < 800; i++) {
+    stars[i] = {
+      x: random(-width, width),
+      y: random(-height, height),
+      z: random(width),
+      pz: null
+    };
+    stars[i].pz = stars[i].z;
+  }
 }
 
 function draw() {
-    speed = map(mouseX, 0, width, 0, 50);
-    background(0);
-    translate(width/2, height/2);
-    for(let i = 0; i < stars.length; i++){
-    stars[i].update();
-    stars[i].show();
-  }
-  
-}
+  speed = map(mouseX, 0, width, 0, 50);
+  background(0);
+  translate(width / 2, height / 2);
 
+  for (let i = 0; i < stars.length; i++) {
+    let star = stars[i];
+
+    star.z -= speed;
+    if (star.z < 1) {
+      star.z = width;
+      star.x = random(-width, width);
+      star.y = random(-height, height);
+      star.pz = star.z;
+    }
+
+    let sx = map(star.x / star.z, 0, 1, 0, width);
+    let sy = map(star.y / star.z, 0, 1, 0, height);
+    let r = map(star.z, 0, width, 16, 0);
+
+    let px = map(star.x / star.pz, 0, 1, 0, width);
+    let py = map(star.y / star.pz, 0, 1, 0, height);
+
+    star.pz = star.z;
+
+    fill(255);
+    noStroke();
+    ellipse(sx, sy, r, r);
+
+    stroke(255);
+    line(px, py, sx, sy);
+  }
+}
