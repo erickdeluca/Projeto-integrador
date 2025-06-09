d81315dc0758a9757e679406269bbcd1f33cdfbf
Erick de Luca <delucaerick@gmail.com>
2025-06-08 22:39:55 -0300

    adiciona interacao do mouse com o obstaculo

--- sketch.js
+++ sketch.js
@@ -1,45 +1,142 @@
-class Star {
-    constructor() {
-        this.x = random(-width, width);
-        this.y = random(-height, height);
-        this.z =  random(width);
-    }
-
-    update(){
-        this.z -= 1;
-        if(this.z < 1){
-            this.z = width;
-            this.x = random(-width, width);
-            this.y = random(-height, height);
-        }
-    }
-    
-    show() {
-        let raio = 2;
-        fill(255); 
-        noStroke();
-        let sx = map(this.x / this.z, 0, 1, 0, width);
-        let sy = map(this.y / this.z, 0, 1, 0, width);
-        circle(sx, sy, raio * 2);
-    }
-}
-
-let stars =  [];
+let stars = [];
+let obstacle = [];
+let speed = 0;
+let slider;
 
 function setup() {
-    createCanvas(800, 800);
-    for(let i = 0; i < 800; i++){
-      stars[i] = new Star();
-    }
+  createCanvas(800, 800);
+  for (let i = 0; i < 2000; i++) {
+    stars[i] = {
+      x: random(-width, width),
+      y: random(-height, height),
+      z: random(width),
+      pz: null
+    };
+    stars[i].pz = stars[i].z;
+  }
+
+for (let i = 0; i < 3; i++) {
+    obstacle[i] = {
+      obstacleX: random(-width, width),
+      obstacleY: random(-height, height),
+      obstacleZ: random(width),
+      obstaclePz: null
+    };
+    obstacle[i].obstaclePz = obstacle[i].obstacleZ;
+  }
+  
+  controleVelocidade();
+  textAlign(CENTER, CENTER);
+  textSize(32);
 }
 
 function draw() {
-    background(0);
-    translate(width/2, height/2);
-    for(let i = 0; i < stars.length; i++){
-    stars[i].update();
-    stars[i].show();
+  speed = map(slider.value(), 0, width, 0, 70);
+  background(0);
+  translate(width / 2, height / 2);
+
+  for (let i = 0; i < stars.length; i++) {
+    let star = stars[i];
+
+    star.z -= speed;
+    if (star.z < 1) {
+      star.z = width;
+      star.x = random(-width, width);
+      star.y = random(-height, height);
+      star.pz = star.z;
+    }
+
+    let sx = map(star.x / star.z, 0, 1, 0, width);
+    let sy = map(star.y / star.z, 0, 1, 0, height);
+    let r = map(star.z, 0, width, 16, 0);
+
+    let px = map(star.x / star.pz, 0, 1, 0, width);
+    let py = map(star.y / star.pz, 0, 1, 0, height);
+
+    star.pz = star.z;
+
+    fill(255);
+    noStroke();
+
+    stroke(255);
+    line(px, py, sx, sy);
   }
-  
+
+  for (let i = 0; i < obstacle.length; i++) {
+    let enemy = obstacle[i];
+
+    enemy.obstacleZ -= speed;
+    if (enemy.obstacleZ < 1) {
+        enemy.obstacleZ = width;
+        enemy.obstacleX = random(-100, 100);
+        enemy.obstacleY = random(-100, 100);
+        enemy.obstaclePz = enemy.obstacleZ;
+    }
+
+    if(speed >= 15.00){
+        enemy -= enemy;
+    }
+
+    let enemySx = map(enemy.obstacleX / enemy.obstacleZ, 0, 1, 0, width);
+    let enemySy = map(enemy.obstacleY / enemy.obstacleZ, 0, 1, 0, height);
+    let enemyR = map(enemy.obstacleZ, 0, width, 16, 0);
+
+    enemy.obstaclePz = enemy.obstacleZ;
+    enemy.enemySx = enemySx;
+    enemy.enemySy = enemySy;
+    enemy.enemyR = enemyR;
+
+    noStroke();
+    fill(255,0, 0);
+    circle (enemySx, enemySy, enemyR * 2);
+
+    fill(87, 87, 87);
+    circle (enemySx, enemySy, enemyR / 2);
+
+    }
+    
+    painelControle();
 }
 
+function controleVelocidade() {
+  slider = createSlider(0, 600, 0);
+  slider.position(width / 2 - 70, 750);
+}
+
+function painelControle() {
+  fill(40, 40, 50);
+  noStroke();
+  circle(0, 1200, 2000);
+
+  fill(20, 20, 30);
+  stroke(50, 100, 150);
+  strokeWeight(3);
+  rect(-200, 230, 400, 200, 20);
+
+  fill(0, 20, 40);
+  stroke(0, 150, 200);
+  strokeWeight(2);
+  rect(-180, 250, 360, 150, 10);
+  
+  fill(255);
+  if (speed.toFixed(2) == 52.50) {
+    text('Velocidade máxima', 0, 300);
+  } else {
+    text(speed.toFixed(2), 0, 300);
+  }
+}
+
+function mousePressed() {
+  for (let i = 0; i < obstacle.length; i++) {
+    if (obstacleClicked(obstacle[i], mouseX, mouseY)) {
+      console.log(".");
+      obstacle[i] -= obstacle[i];
+      break;
+    }
+  }
+}
+
+function obstacleClicked(enemy, mx, my) {
+  let d = dist(mx - width / 2, my - height / 2, enemy.enemySx, enemy.enemySy);
+  return d < enemy.enemyR;
+}
